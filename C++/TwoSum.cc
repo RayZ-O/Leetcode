433ae38e25001b0b95e21d2be71922ee76aa82c8
RayZ-O <rayzhang1011@gmail.com>
2015-05-02 00:40:20 -0400

    <C++> Two Sum

---
+++ C++/TwoSum.cc
@@ -0,0 +1,35 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Given an array of integers, find two numbers such that they add up to a specific target number.
+// The function twoSum should return indices of the two numbers such that they add up to the target, 
+// where index1 must be less than index2. Please note that your returned answers(both index1 and index2) 
+// are not zero-based.
+// Assume that each input would have exactly one solution
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // key is the number in nums, value is the corresponding index
+        unordered_map<int, int> hashtable;
+        vector<int> result;
+        int size = nums.size();
+        for (int i = 0; i < size; ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if(it != hashtable.end()) {
+                result = { i + 1, it->second + 1 };
+                break;
+            } else {
+                hashtable.emplace(nums[i], i);
+            }
+        }
+        if(result[0] > result[1]) {
+            swap(result[0], result[1]);
+        }
+        return result;
+    }
+};
