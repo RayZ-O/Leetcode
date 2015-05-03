9c2593a4b6fec273eac8a5dfa4c96c5ceb6e4bf0
Rui Zhang <rayzhang1011@gmail.com>
2015-05-03 04:02:27 +0000

    TwoSum.cc edited, Don't need to swap result

--- C++/TwoSum.cc
+++ C++/TwoSum.cc
@@ -21,14 +21,11 @@ public:
         for (int i = 0; i < size; ++i) {
             auto it = hashtable.find(target - nums[i]);
             if(it != hashtable.end()) {
-                result = { i + 1, it->second + 1 };
+                result = { it->second + 1, i + 1 };
                 break;
             } else {
                 hashtable.emplace(nums[i], i);
             }
-        }
-        if(result[0] > result[1]) {
-            swap(result[0], result[1]);
         }
         return result;
     }
