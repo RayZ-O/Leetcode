0626b4b5a47a48cd85ac3eeba85598aa443ab697
Rui Zhang <rayzhang1011@gmail.com>
2015-05-03 04:03:53 +0000

    TwoSum.cc edited, delete header

--- C++/TwoSum.cc
+++ C++/TwoSum.cc
@@ -1,10 +1,3 @@
-#include <vector>
-#include <unordered_map>
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
 // Given an array of integers, find two numbers such that they add up to a specific target number.
 // The function twoSum should return indices of the two numbers such that they add up to the target, 
 // where index1 must be less than index2. Please note that your returned answers(both index1 and index2) 
