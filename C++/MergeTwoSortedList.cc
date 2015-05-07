9912337cf6ac7daf7ce77253d9b2167b29c0f2db
ubuntu <rayzhang1011@gmail.com>
2015-05-08 02:24:14 +0800

    Fixed ident

--- C++/MergeTwoSortedList.cc
+++ C++/MergeTwoSortedList.cc
@@ -13,46 +13,46 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
- 		if (!l1) {
-			return l2;
-		} else if (!l2) {
-			return l1;
-		}
-		// traverser
-		ListNode *p1 = l1;
-		ListNode *p2 = l2;
-		// head of unsorted portion
-		ListNode *h1 = l1;
-		ListNode *h2 = l2;
-		ListNode *head = new ListNode(0);
-		ListNode *sorted = head;    // tail of sorted portion
-		ListNode *current = head;   // previous node of current traverser
-		while (p1 && p2) {
-			if (p1->val < p2->val) {
-				do {
-					current = p1;
-					p1 = p1->next;
-				} while (p1 && p1->val < p2->val);
-				sorted->next = h1;
-				h1 = p1;
-			} else {
-				do {
-					current = p2;
-					p2 = p2->next;
-				} while (p2 && p1->val >= p2->val);	
-				sorted->next = h2;
-				h2 = p2;
-			}			
-			sorted = current;
-		}
-		if (!p1) {
-			sorted->next = p2;
-		} else if (!p2) {
-			sorted->next = p1;
-		}
-		ListNode *result = head->next;
-		head->next = nullptr;
-		delete head;
-		return result;
+        if (!l1) {
+	    return l2;
+        } else if (!l2) {
+	    return l1;
+        }
+	// traverser
+	ListNode *p1 = l1;
+	ListNode *p2 = l2;
+	// head of unsorted portion
+	ListNode *h1 = l1;
+	ListNode *h2 = l2;
+	ListNode *head = new ListNode(0);
+	ListNode *sorted = head;    // tail of sorted portion
+	ListNode *current = head;   // previous node of current traverser
+	while (p1 && p2) {
+	    if (p1->val < p2->val) {
+	        do {
+		    current = p1;
+		    p1 = p1->next;
+		} while (p1 && p1->val < p2->val);
+		sorted->next = h1;
+		h1 = p1;
+	    } else {
+                do {
+		    current = p2;
+		    p2 = p2->next;
+		} while (p2 && p1->val >= p2->val);	
+		sorted->next = h2;
+		h2 = p2;
+	    }			
+	    sorted = current;
+	}
+	if (!p1) {
+	    sorted->next = p2;
+	} else if (!p2) {
+	    sorted->next = p1;
+	}
+	ListNode *result = head->next;
+	head->next = nullptr;
+	delete head;
+	return result;
     }
 };
