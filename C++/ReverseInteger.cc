2c00882893cb8eeaea93452e133e24a4d52b6665
ubuntu <rayzhang1011@gmail.com>
2015-05-11 06:40:07 +0800

    modify reverse integer

--- C++/ReverseInteger.cc
+++ C++/ReverseInteger.cc
@@ -3,30 +3,25 @@
 // Example1: x = 123, return 321
 // Example2: x = -123, return -321 
 
+
 class Solution {
 public:
     int reverse(int x) {
         int rev = 0;
-        while (x != 0 && rev < 99999999 && rev > -99999999) {
-            rev *= 10;
-            rev += x % 10;
-            x /= 10;          
-        } 
-        if(x) {
-            while (x != 0) {
-                int temp = rev * 10;
-                if (temp/10 != rev) {
-                    return 0;
-                }
-                rev = temp;
-                temp = rev + x % 10;
-                if (temp - x % 10 != rev) {
-                    return 0;
-                }
-                rev = temp;
-                x /= 10; 
-            }
+        int sign = 1;
+        if (x < 0) {
+            sign = -1;
+            x = -x;
         }
-        return rev;
+        while (x != 0) {
+            int digit = x % 10;
+            if ((INT_MAX - digit) / 10 < rev) {
+                return 0;
+            }
+            rev = rev * 10 + digit;
+            x /= 10;          
+        }     
+        return sign * rev;
     }
 };
+
