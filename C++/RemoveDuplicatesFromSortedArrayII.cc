e767eca921e1ea0071cc329a99d3f260ba6df286
ubuntu <rayzhang1011@gmail.com>
2015-05-11 20:41:09 +0800

    <C++> Remove Duplicates from Sorted Array II

---
+++ C++/RemoveDuplicatesFromSortedArrayII.cc
@@ -0,0 +1,36 @@
+// Follow up for "Remove Duplicates":
+// What if duplicates are allowed at most twice?
+
+// For example,
+// Given sorted array nums = [1,1,1,2,2,3],
+
+// Your function should return length = 5, with the first five elements of nums 
+// being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length. 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        if(!size) {
+            return 0;
+        }
+        int val = nums[0];
+        int distinct = 0;
+        int count = 1;
+        for (int i = 1; i < size; ++i) {
+            if (nums[i] == val) {
+                if (count == 1) {
+                    count++;
+                    distinct++;                
+                    nums[distinct] = nums[i];
+                } 
+            } else {
+                count = 1;
+                val = nums[i];
+                distinct++;
+                nums[distinct] = nums[i];
+            }
+        }
+        return distinct + 1;
+    }
+};
