d46f24eac52ab7afe0c77ccf40060b2120419b23
RayZ_O <rayzhang1011@gmail.com>
2015-09-10 13:08:13 -0400

    <C++> Same Tree

---
+++ C++/SameTree.cc
@@ -0,0 +1,13 @@
+// Given two binary trees, write a function to check if they are equal or not.
+
+// Two binary trees are considered equal if they are structurally identical and 
+// the nodes have the same value. 
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        else if (p && q && p->val == q->val) return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        else return false;
+    }
+};
