015d8f4b126cda9076818103fa92983d7247a6b1
RayZ_O <rayzhang1011@gmail.com>
2015-09-29 19:10:32 -0400

    <C++> Climbing Stairs

---
+++ C++/ClimbingStairs.cc
@@ -0,0 +1,16 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n < 1) return 0;
+        if (n == 1) return 1;
+        vector<int> nums{1, 2};
+        for (int i = 2; i < n; i++) {
+            nums.push_back(nums[i-1] + nums[i-2]);
+        }
+        return nums.back();
+    }
+};
