274b3bbcb82912901eaf82508bcf35cdfa6c28d0
RayZ_O <rayzhang1011@gmail.com>
2015-09-29 20:22:22 -0400

    <C++> Peeking Iterator

---
+++ C++/PeekingIterator.cc
@@ -0,0 +1,61 @@
+// Given an Iterator class interface with methods: next() and hasNext(), design and implement a
+// PeekingIterator that support the peek() operation -- it essentially peek() at the element that
+// will be returned by the next call to next().
+
+// Here is an example. Assume that the iterator is initialized to the beginning of the list: [1, 2, 3].
+
+// Call next() gets you 1, the first element in the list.
+
+// Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+
+// You call next() the final time and it returns 3, the last element. Calling hasNext() after that
+// should return false.
+
+class Iterator {
+    struct Data;
+    Data* data;
+public:
+    Iterator(const vector<int>& nums);
+    Iterator(const Iterator& iter);
+    virtual ~Iterator();
+    // Returns the next element in the iteration.
+    int next();
+    // Returns true if the iteration has more elements.
+    bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+    int current;
+    bool notEnd;
+public:
+    PeekingIterator(const vector<int>& nums) : Iterator(nums), notEnd(false) {
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+        if (notEnd = Iterator::hasNext()) {
+            current = Iterator::next();
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    int peek() {
+        return current;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
+        int tmp = current;
+        if (Iterator::hasNext()) {
+            current = Iterator::next();
+        } else {
+            notEnd = false;
+        }
+        return tmp;
+    }
+
+    bool hasNext() const {
+        return notEnd;
+    }
+};
