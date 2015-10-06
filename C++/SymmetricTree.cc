42673d10c18e0aed6169c0aff58f90515b3fc37e
RayZ_O <rayzhang1011@gmail.com>
2015-10-06 17:14:01 -0400

    Iterative symmetric tree

--- C++/SymmetricTree.cc
+++ C++/SymmetricTree.cc
@@ -24,8 +24,35 @@ class Solution {
 
     }
 public:
-    bool isSymmetric(TreeNode* root) {
+    // recursive
+    bool isSymmetricr(TreeNode* root) {
         if (!root) return true;
         return compare(root->left, root->right);
+    }
+
+    // iterative
+    bool isSymmetrici(TreeNode* root) {
+        if (!root) return true;
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        st1.push(root->left);
+        st2.push(root->right);
+        while (!st1.empty() || !st2.empty()) {
+            TreeNode *n1 = st1.top();
+            TreeNode *n2 = st2.top();
+            if ((!n1 && n2) || (n1 && !n2))
+                return false;
+            else if (n1 && n2 && n1->val != n2->val)
+                return false;
+            st1.pop();
+            st2.pop();
+            if (n1) {
+                st1.push(n1->left);
+                st1.push(n1->right);
+                st2.push(n2->right);
+                st2.push(n2->left);
+            }
+        }
+        return st1.empty() && st2.empty();
     }
 };
