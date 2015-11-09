0a06c3fbfab8c99c05c4bb7d031524aeb7a2c945
RayZ_O <rayzhang1011@gmail.com>
2015-11-08 21:18:36 -0500

    <C++> Next Permutation

---
+++ C++/NextPermutation.cc
@@ -0,0 +1,33 @@
+// Implement next permutation, which rearranges numbers into the
+// lexicographically next greater permutation of numbers.
+
+// If such arrangement is not possible, it must rearrange it as
+// the lowest possible order (ie, sorted in ascending order).
+
+// The replacement must be in-place, do not allocate extra memory.
+
+// Here are some examples. Inputs are in the left-hand column and
+// its corresponding outputs are in the right-hand column.
+// 1,2,3 → 1,3,2
+// 3,2,1 → 1,2,3
+// 1,1,5 → 1,5,1
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() < 2)
+            return;
+        int i = nums.size() - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
+            i--;
+        if (i < 0) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int j = nums.size() - 1;
+        while (j > i && nums[i] >= nums[j])
+            j--;
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
