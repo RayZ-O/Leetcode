c97b66ae6e10ce84b10c75147a9341b43e84d5aa
Rui Zhang <rayzhang1011@gmail.com>
2016-03-15 22:11:37 -0400

    <C++> House Robber III

---
+++ C++/HouseRobberIII.cc
@@ -0,0 +1,38 @@
+// The thief has found himself a new place for his thievery again. There is only one entrance to this area,
+//  called the "root." Besides the root, each house has one and only one parent house. After a tour, the 
+//  smart thief realized that "all houses in this place forms a binary tree". It will automatically contact 
+//  the police if two directly-linked houses were broken into on the same night.
+
+// Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+// Example 1:
+//      3
+//     / \
+//    2   3
+//     \   \ 
+//      3   1
+// Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+// Example 2:
+//      3
+//     / \
+//    4   5
+//   / \   \ 
+//  1   3   1
+// Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        auto p = robTree(root);
+        return max(p.first, p.second);
+    }
+    
+    pair<int, int> robTree(TreeNode* root) {
+        if (!root){
+            return {0, 0};
+        }
+        auto l = robTree(root->left);
+        auto r = robTree(root->right);
+        return {l.second + r.second + root->val, max(l.first, l.second) + max(r.first, r.second)};
+    }
+};
